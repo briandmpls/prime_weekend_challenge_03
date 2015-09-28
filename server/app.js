36dfad53c81e29096dae8a1d28f0b8e71160623a
briandmpls <briandmpls@gmail.com>
2015-09-28 09:24:40 -0500

    heroku port update

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.get('/getStudents',function(require,response){
 });
 
 
-var server = app.listen(process.env.port, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
     console.log('listening on port:',port);
 });
